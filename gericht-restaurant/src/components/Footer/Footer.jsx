daa5fa07a389c5ffd8771ea871f66189cdbcb578
Nicitaa <icpcsenondaryemail@gmail.com>
2023-05-03 21:39:26 +0200

    <Footer/> done
    without media-queries

--- gericht-restaurant/src/components/Footer/Footer.jsx
+++ gericht-restaurant/src/components/Footer/Footer.jsx
@@ -1,5 +1,69 @@
+import './footer.css'
+
 export function Footer () {
 return (
-<div className="footer">Footer</div>
+<div className="footer">
+ 
+<div className="footer-bottom">
+<p className="subTitle">2021 Gericht. All Rights reserved.</p>
+</div>
+
+ <div className="footer-top">
+  
+
+<div className="footer-top_info">
+<h1 className="footer-title">Contact us</h1>
+<p className="subTitle footer-subTitle">9 W 53rd St, New York, NY 10019, USA</p>
+<ul className='subTitle footer-subTitle'>
+  <li><a href="tel:+1 212-344-1230">+1 212-344-1230</a></li>
+  <li><a href="tel:+1 212-555-1230">+1 212-555-1230</a></li>
+</ul>
+</div>
+
+<div className="footer-top_center">
+  <h1 className="title footer-logo-text">Gericht</h1>
+
+  <p className='footer-description'>"The best way to find yourself is to lose yourself in the service of others."</p>
+
+  <svg className='spoon-svg'>
+  <use xlinkHref='./sprite.svg#spoon'/>
+  </svg>
+
+ <div className='socials'>
+ <svg className='social-svg'>
+  <use xlinkHref='./sprite.svg#facebook'/>
+  </svg>
+
+  <svg className='social-svg'>
+  <use xlinkHref='./sprite.svg#twitter'/>
+  </svg>
+
+  <svg className='social-svg'>
+  <use xlinkHref='./sprite.svg#instagram'/>
+  </svg>
+
+ </div>
+
+</div>
+
+  <div className="footer-top_info">
+<h1 className="footer-title">Working Hours</h1>
+  <p className="subTitle footer-subTitle">Monday-Friday<br/>
+    08:00 am - 12:00 am
+  </p>
+  <p className="subTitle footer-subTitle">Saturday-Sunday<br/>
+    07:00 am - 11:00 am
+  </p>
+  </div>
+
+ </div>
+
+
+
+<div className="footer-bottom">
+<p className="subTitle">2021 Gericht. All Rights reserved.</p>
+</div>
+
+</div>
 )
 }
--- gericht-restaurant/src/components/Navbar/Navbar.tsx
+++ gericht-restaurant/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,30 @@
 import './navbar.css'
 import { navLinks } from "../../constant";
 import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { motion,useAnimationControls } from 'framer-motion'
+import {gsap} from 'gsap'
+import { animate } from 'framer-motion/dom';
 
 export function Navbar () {
+  const [showMobileLinks,setShowMobileLinks] = useState(false)
+
+  const mobileMenuRef = useRef(null);
+  function closeMobileLinks() {
+    const mobileMenu = mobileMenuRef.current;
+    gsap.to(mobileMenu, {
+      duration: 1,
+      y:-1920,
+      onComplete: () => setShowMobileLinks(false),
+    });
+  }
+
+
+  
+    
+    
+
+  
 return (
 <div className="navbar">
   <img className='navbar-logo' src="./logo.png"/>
@@ -15,9 +37,35 @@ return (
 
       <div className='navbar-auth'>
           <span><a onClick={() => {/* modal auth */}}>Log In / Registration</a></span>
-        <div />
+        <div className='line'/>
           <span><a onClick={() => {/* modal book a table */}}>Book Table</a></span>
       </div>
+
+      <svg className='navbar-mobile-icon' onClick={() => setShowMobileLinks(true)}>
+            <use xlinkHref='./sprite.svg#hamburger-menu'/>
+      </svg>
+
+      {showMobileLinks && 
+      <motion.div className='navbar-mobile-menu' animate={{y:[-1200,0]}} ref={mobileMenuRef}
+      transition={{ type: "spring",duration:0.75}}>
+         <ul className="navbar-links-mobile">
+           {navLinks.map(navLink => (
+           <Link className='li-item' to={navLink.to} key={navLink.id}>{navLink.title}</Link>
+          ))}
+        </ul>
+        <div className='navbar-auth-mobile'>
+          <span><a onClick={() => {/* modal auth */}}>Log In / Registration</a></span>
+        <div className='line'/>
+          <span><a onClick={() => {/* modal book a table */}}>Book Table</a></span>
+        </div>
+        <svg style={{position:'absolute',top:'3rem',right:'3rem',width:'2rem',height:'2rem',fill:'#ffffff'}}
+        onClick={() => closeMobileLinks()}>
+            <use xlinkHref='./sprite.svg#close'/>
+        </svg>
+      </motion.div>}
+    
+
+      
 
 </div>
 )
