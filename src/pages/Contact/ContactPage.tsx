4a83009276acd4e9d9066d1bb9a5d2ff849b81d7
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-05 21:49:49 +0200

    pages responsive

--- src/pages/Contact/ContactPage.tsx
+++ src/pages/Contact/ContactPage.tsx
@@ -3,7 +3,7 @@ import { BsDiscord } from 'react-icons/bs'
 
 export function ContactPage() {
   return (
-    <div className="flex flex-col justify-center items-center pt-32">
+    <div className="flex flex-col justify-center items-center pt-96 4K:pt-64 LaptopL:pt-32">
       <h1 className="title">Contact</h1>
       <p className="subTitle mb-4">9 W 53rd St, New York, NY 10019, USA      </p>
       <ul>
